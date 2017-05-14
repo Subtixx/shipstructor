0685c6f3532083c730fa9c23430198b00e5ea6ff
Royal~Jelly <joshuaannandoolittle@gmail.com>
2017-05-14 16:07:29 -0500

    tried a thing but im still really bad at lua [Error] Exception while invoking lua function 'shipthetherGUI'. (LuaException) Error code 2, [string "/scripts/shiptethergui.lua"]:21: attempt to index a nil value (global 'entity')

--- scripts/shiptethergui.lua
+++ scripts/shiptethergui.lua
@@ -1,20 +1,28 @@
--- This script is highy experimental please help if you know how to lua better than I
+--[[ 
 
--- The Ship tether will work as follows (planed not implemented):
+This script is highy experimental please help if you know how to lua better than I
 
--- 1) player will insert valid ship pod
--- 2) Big red button in GUI can now be pressed to start ship replacement
--- 3) Cinimatic of variable length being played (based on a value stored in provided ship pod)
--- 4) shit tether now scans the shipworld  (the scan is stored in a new ship pod)
--- 5) ship tether breaks ship (and prints the contents of the orriginal pod)
--- 6) cinimatic ends (player now has shiney new ship)
+The Ship tether will work as follows (planed not implemented):
 
+1) player will insert valid ship pod
+2) Big red button in GUI can now be pressed to start ship replacement
+3) Cinimatic of variable length being played (based on a value stored in provided ship pod)
+4) shit tether now scans the shipworld  (the scan is stored in a new ship pod)
+5) ship tether breaks ship (and prints the contents of the orriginal pod)
+6) cinimatic ends (player now has shiney new ship)
+
+--]]
 
 
 -- on button press check if the item is a valid ship pod (read a value in the item)
 function shipthetherGUI()
-
 -- If not a ship pod give player the invalid item back
+-- logs item params i think
+    local items = world.containerItems(entity.id())
+    for k, v in pairs(items) do
+        world.logInfo("Item: %s", v)
+    end
+
 
 -- If a ship pod then trigger new function
 end
