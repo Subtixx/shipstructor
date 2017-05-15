9df3ba9343c0eb634ee31b3f275213f7951555e9
Royal~Jelly <joshuaannandoolittle@gmail.com>
2017-05-15 06:46:45 -0500

    Ship Modules/Controllers , Wip implementation If the Modules in a box data gets stored in tehm they crash for some odd reason

--- scripts/shiptethergui.lua
+++ scripts/shiptethergui.lua
@@ -1,15 +1,15 @@
---[[ 
+--[[
 
-This script is highy experimental please help if you know how to lua better than I
+This script is highly experimental please help if you know how to lua better than I
 
 The Ship tether will work as follows (planed not implemented):
 
 1) player will insert valid ship pod
 2) Big red button in GUI can now be pressed to start ship replacement
-3) Cinimatic of variable length being played (based on a value stored in provided ship pod)
-4) shit tether now scans the shipworld  (the scan is stored in a new ship pod)
-5) ship tether breaks ship (and prints the contents of the orriginal pod)
-6) cinimatic ends (player now has shiney new ship)
+3) cinematic of variable length being played (based on a value stored in provided ship pod)
+4) shit tether now scans the ship world  (the scan is stored in a new ship pod)
+5) ship tether breaks ship (and prints the contents of the original pod)
+6) cinematic ends (player now has shinny new ship)
 
 --]]
 
@@ -22,5 +22,5 @@ function shipthetherGUI()
 end
 
 function shiptetherCinematic()
--- Play a Cinimatic for a certian amount of time based on "cinimaticLength" stored in item file 
+-- Play a Cinematic for a certain amount of time based on "cinematicLength" stored in item file
 end
